1feb59ae1d640c3fc87fbb1856ce40a03d4620bc
LORD M <128736957+Lord-MonM@users.noreply.github.com>
2025-05-16 22:49:29 +0100

    installing middlewares

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express"
+import bodyParser from "body-parser"
+import cookieSession from "cookie-session"
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieSession({
+ keys: ["kmsmodisoaop"]
+}))
