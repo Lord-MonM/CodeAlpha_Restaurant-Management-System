cd6774124e015c67b448d22d691e617cdecd055a
LORD M <128736957+Lord-MonM@users.noreply.github.com>
2025-06-05 02:28:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cookieSession = require("cookie-session");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
@@ -13,9 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.use(cookieSession({
-//  keys: ["kmsmodisoaop"]
-// }));
+
 app.use("/api/orders",require("./routes/orderRoutes.js"));
 app.use("/api/reports",require("./routes/reportRoutes.js"));
 app.use("/api/menu",require("./routes/menuRoutes.js"));
